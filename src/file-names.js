c3dd2281710238460788104e882ef2045f5c9590
Maria Ermolaeva <maria.kadantseva@gmail.com>
2022-10-13 17:32:50 +0300

    feat: solutons depth and file-names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(arr) {
+  return arr.reduce((res,item) => {
+    let i = 1;
+    let str = `${item}(${i})`
+    if(!res.includes(item)) {
+      res.push(item)
+    } else {
+      if(res.includes(str)) {
+        str = `${item}(${++i})`;
+      }
+      res.push(str)
+    }
+    return res
+    }, [])
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,11 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+    constructor() {
+      this.el = []}
+
+  calculateDepth(arr) {  
+    let max = 0
+    let count = 1
+      for (let i = 0; i < arr.length; i++) {
+        if(arr[i] instanceof(Array)) {
+          max = Math.max(this.calculateDepth(arr[i]), max)
+        }
+      } return max + count
   }
 }
+
 
 module.exports = {
   DepthCalculator
